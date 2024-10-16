df259ec3352e93173d52792cb99b2d7d0995319c
Dev_Chaz <hajjaldiimraif@gmail.com>
2024-10-16 19:15:47 +0800

    final modification

--- Quiz/problem2.m
+++ Quiz/problem2.m
@@ -3,20 +3,27 @@
 data = readtable('sales_data.xlsx');
 %disp(data.Properties.VariableNames); % Option to check for column names 
 
-data.QuantitySold_in_KG_ = data.QuantitySold_in_MT_ * 1000; 
+data.QuantitySold_in_KG_ = data.QuantitySold_in_MT_ * 1000;
 
-data.TotalSales = data.QuantitySold_in_MT_ .* data.UnitPrice_PHP_; 
+data.TotalSales = data.QuantitySold_in_KG_ .* data.UnitPrice_PHP_;
 
 fileID = fopen('sales_summary.txt', 'w');
 
+function formatted_amount = format_to_peso(amount)
+    formatted_amount = sprintf('₱ %.2f', amount);
+    formatted_amount = regexprep(formatted_amount, '(\d)(?=(\d{3})+(?!\d))', '$1,');
+end
+
 for i = 1:height(data)
-    fprintf(fileID, 'ProductID: %s, Total Sales: %.2f, Quantity Sold: %.2f MT\n', data.ProductID{i}, data.TotalSales(i), data.QuantitySold_in_MT_(i));
+    fprintf(fileID, 'ProductID: %s, Total Sales: %s, Quantity Sold: %.2f MT\n', ...
+        data.ProductID{i}, format_to_peso(data.TotalSales(i)), data.QuantitySold_in_MT_(i));
 end
 
 [max_sales, max_index] = max(data.TotalSales);
 max_sales_id = data.ProductID{max_index};  
 
-fprintf(fileID, 'Product with Highest Sales: %s, Sales Amount: %.2f\n', max_sales_id, max_sales);
+fprintf(fileID, 'Product with Highest Sales: %s, Sales Amount: %s\n', ...
+    max_sales_id, format_to_peso(max_sales));
 
 fclose(fileID);
 
